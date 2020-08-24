a368476231767bd2c9bf78aec89dbb82a4c53631
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-08-24 17:31:39 -0500

    added render test for header component

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Header from "./Header";
+
+test("renders correct content", () => {
+    const { getByText } = render(<Header />);
+
+    getByText("Employee Directory");
+    getByText("Browse employees by name or use search bar to find employee info.")
+})
