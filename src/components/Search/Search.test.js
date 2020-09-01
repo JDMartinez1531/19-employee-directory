e8a783fd9d3f892c2c40bf8c4d204c3014993f6d
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-09-01 15:59:04 -0500

    added first search bar test

---
+++ src/components/Search/Search.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Search from ".";
+
+
+it("should render with placeholder text", () => {
+    const {getByPlaceholderText, baseElement} = render(<Search />);
+
+    expect(baseElement).toBeInTheDocument();
+    // getByPlaceholderText("Search");
+})
