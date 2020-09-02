fd8a08356f32639f79d51ae9cf06500b9392e488
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-09-02 12:01:45 -0500

    added input test to search bar

--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event"
 import Search from ".";
 
 
-it("should render with placeholder text", () => {
+it("should render with placeholder text and receive user input", () => {
     const {getByPlaceholderText, baseElement} = render(<Search />);
 
     expect(baseElement).toBeInTheDocument();
     getByPlaceholderText("Search");
+    userEvent.type(getByPlaceholderText("Search"), "Hello, World!");
+    expect(getByPlaceholderText("Search")).toHaveValue("Hello, World!");
 })
